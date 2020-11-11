ee4afa29b8e39e8bf1f46b613b4696cc938ee560
Dalmiro <dalmirozant@gmail.com>
2020-11-11 02:58:56 -0300

    Corregir errores

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 if(process.env.NODE_ENV === 'dev'){
    urlDB = 'mongodb://localhost:27017/boliche';  
 } else {
-    urlDB = proces.env.MONGO_URI;
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
