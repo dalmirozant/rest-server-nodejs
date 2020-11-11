e1124e477abdd40b149451e0023df7403cfc9e52
Dalmiro <dalmirozant@gmail.com>
2020-11-11 02:55:47 -0300

    Agregar variables de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 if(process.env.NODE_ENV === 'dev'){
    urlDB = 'mongodb://localhost:27017/boliche';  
 } else {
-    urlDB = 'mongodb+srv://tutorial:tuto2020@cluster0.im7xu.gcp.mongodb.net/boliche?retryWrites=true&w=majority';
+    urlDB = proces.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
